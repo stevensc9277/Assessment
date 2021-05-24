e720eee38899f7e2d15147b64a3799e5b0a2aa57
Caleb Stevens <>
2021-05-24 22:53:55 +0000

    need to verify shape (need to check number of sides)

--- 02_string_check.py
+++ 02_string_check.py
@@ -1,8 +1,22 @@
+# checks that there are no numbers in the string for user input
 def shape():
   error = "Please enter a valid shape"
-
+ 
   while True:
-    try:
-      polygon = input("What is the shape you are trying to solve for? ")
+    has_number = False
+    polygon = input("What is the shape you are trying to solve for? ")
 
-      if 
+    for i in polygon:
+      if i.isdigit():
+        has_number = True
+        
+    if has_number == True:
+      print(error)
+
+    else:
+      return polygon
+
+appearance = shape()
+
+
+        
