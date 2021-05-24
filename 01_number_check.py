04f1216d45dec3d8a084d1a2fd86775c588e6218
Caleb Stevens <>
2021-05-24 21:36:26 +0000

    checks number from user input

---
+++ 01_number_check.py
@@ -0,0 +1,22 @@
+# Checks that user input (number) is valid 
+def num_check(question, low, type):
+  error = "Please enter a number more than 0"
+
+  while True:
+    try:
+      response = type(input(question))
+
+      if response <= 0:
+        print(error)
+        print()
+      
+      else:
+        return response
+    
+    except ValueError:
+      print(error)
+      print()
+
+# test
+side_a = num_check("Enter a length: ", 1, float)
+print(side_a)
