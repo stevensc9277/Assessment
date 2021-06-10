86b5890eb282d9a22d81f79d53f725a3f974901b
Caleb Stevens <60583258+stevensc9277@users.noreply.github.com>
2021-06-10 00:30:03 +0000

    successfully draws a triangle with given lengths and angles

--- drawing.py
+++ drawing.py
@@ -19,13 +19,14 @@ def triangle(displacement, angle):
   tess.forward(displacement[0] * 10)  # Make tess draw a triangle
   tess.left(180 - angle[0])
   turtle.setpos(displacement[1] * 10, 0)
-  turtle.write('B', move = True, font = 'style')
+  turtle.write('B', font = 'style', align = 'left')
+  turtle.setpos(displacement[1]*10, 0)
   tess.forward(displacement[1] * 10)
   tess.left(180 - angle[1])
-  turtle.setpos(displacement[2]* 5, 86.6)
+  turtle.setpos(displacement[2]* 5, 86) # y-axis is vertical height, x is half of line AB, need to function to find y (constantly changes)
   turtle.write('C', move = True, font = 'style', align = 'center')
   tess.forward(displacement[2] * 10)
-  tess.left(180 - angle[2])  # Complete the triangle
+   # Complete the triangle
 
   # tess.right(180)  # Turn tess around
   # tess.forward(80)  # Move her away from the origin
@@ -34,4 +35,4 @@ def triangle(displacement, angle):
   wn.mainloop()
 
 
-to_draw = triangle([10, 10, 10], [60, 60, 60])
+to_draw = triangle([10, 10, 10], [60, 60])
