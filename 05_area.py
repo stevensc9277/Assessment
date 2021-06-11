6792ad0716e919fc28918e61da924ca46b9e6079
Caleb Stevens <60583258+stevensc9277@users.noreply.github.com>
2021-06-11 00:17:54 +0000

    decided not to make function, considered it pointless since it was only a few lines long

---
+++ 05_area.py
@@ -0,0 +1,30 @@
+import math
+
+# Checks that user input (number) is valid 
+def num_check(question, low, high, type):
+  error = "Please enter a number more than {} or less than {}".format(low, high)
+
+  while True:
+    try:
+      response = type(input(question))
+
+      if response <= 0:
+        print(error)
+        print()
+      
+      elif response >= 180:
+        print(error)
+        print()
+      else:
+        return response
+    
+    except ValueError:
+      print(error)
+      print()
+
+side_a = num_check("How long? ", 0, 1000, float)
+side_b = num_check("How long? ", 0, 1000, float)
+inc_angle = num_check("What is the inclusive angle? ", 0, 150, int)
+height = side_b*math.sin(math.radians(inc_angle))
+area = 0.5 * height * side_a
+print("Area = {:.2f}cm^2".format(area))
